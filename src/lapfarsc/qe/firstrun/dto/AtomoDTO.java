ed99a93dd53ca5098a2ed23a2aa1129d70ce6e3c
tuy (Guilherme Mendes) <tuypoa@gmail.com>
2022-02-16 20:35:32 -0300

    test eclipse commit

---
+++ src/lapfarsc/qe/firstrun/dto/AtomoDTO.java
@@ -0,0 +1,37 @@
+package lapfarsc.qe.firstrun.dto;
+
+public class AtomoDTO {
+
+	private String elemento;
+	private Float posicaoX;
+	private Float posicaoY;
+	private Float posicaoZ;
+	
+	public String getElemento() {
+		return elemento;
+	}
+	public void setElemento(String elemento) {
+		this.elemento = elemento;
+	}
+	public Float getPosicaoX() {
+		return posicaoX;
+	}
+	public void setPosicaoX(Float posicaoX) {
+		this.posicaoX = posicaoX;
+	}
+	public Float getPosicaoY() {
+		return posicaoY;
+	}
+	public void setPosicaoY(Float posicaoY) {
+		this.posicaoY = posicaoY;
+	}
+	public Float getPosicaoZ() {
+		return posicaoZ;
+	}
+	public void setPosicaoZ(Float posicaoZ) {
+		this.posicaoZ = posicaoZ;
+	}
+	
+	
+	
+}
---
+++ src/lapfarsc/qe/firstrun/dto/CelulaDTO.java
@@ -0,0 +1,5 @@
+package lapfarsc.qe.firstrun.dto;
+
+public class CelulaDTO {
+
+}
