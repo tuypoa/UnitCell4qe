a85e2c07271543f26e0b20251a3670c6cc09e02c
tuy (Guilherme Mendes) <tuypoa@gmail.com>
2022-02-16 20:14:03 -0300

    Init

---
+++ src/lapfarsc/qe/firstrun/InitFirstRun.java
@@ -0,0 +1,11 @@
+package lapfarsc.qe.firstrun;
+
+public class InitFirstRun {
+
+	public static void main(String[] args) throws Exception{		
+		//System.out.println("InitFirstRun...");
+		
+		//System.out.println("END.");
+	}
+	
+}
